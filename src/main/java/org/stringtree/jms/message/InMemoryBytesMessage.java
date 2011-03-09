38d2cc597c9a981ea64539345fd9b235826986cf
Frank Carver <fcarver@.stl.int>
2011-03-09 12:17:02 +0000

    basic stub implementations of BytesMessage and StreamMessage to clean some old TODOs

--- src/main/java/org/stringtree/jms/message/InMemoryBytesMessage.java
+++ src/main/java/org/stringtree/jms/message/InMemoryBytesMessage.java
@@ -1,143 +1,314 @@
 package org.stringtree.jms.message;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 import javax.jms.BytesMessage;
 import javax.jms.JMSException;
+import javax.jms.MessageNotReadableException;
+import javax.jms.MessageNotWriteableException;
 
 public class InMemoryBytesMessage extends InMemoryMessage implements BytesMessage {
+	private static final int WRITE = 0;
+	private static final int READ = 1;
+	
+	private ByteArrayOutputStream data;
+	private DataOutputStream out;
+	private DataInputStream in;
+	private int mode;
+	
+	public InMemoryBytesMessage() {
+		data = new ByteArrayOutputStream();
+		out = null;
+		in = null;
+		mode = WRITE;
+	}
+	
+	private void ensureReadMode() throws JMSException {
+		if (mode != READ) throw new MessageNotReadableException("attempt to read from message in write mode, use 'reset()' to switch");
+	}
+	
+	private void ensureWriteMode() throws JMSException {
+		if (mode != WRITE) throw new MessageNotWriteableException("attempt to write to message in read mode, use 'clearBody()' to switch");
+	}
 
-    public boolean readBoolean() {
-        // TODO Auto-generated method stub
-        return false;
+	@Override
+    public void reset() throws JMSException {
+    	mode = READ;
+    	out = null;
+    	in = new DataInputStream(new ByteArrayInputStream(data.toByteArray()));
     }
 
-    public byte readByte() {
-        // TODO Auto-generated method stub
-        return 0;
+	@Override
+    public void clearBody() {
+    	mode = WRITE;
+    	data.reset();
+    	out = new DataOutputStream(data);
+    	in = null;
     }
 
-    public int readBytes(byte[] arg0) {
-        // TODO Auto-generated method stub
-        return 0;
+	@Override
+    public boolean readBoolean() throws JMSException {
+    	ensureReadMode();
+    	try {
+			return in.readBoolean();
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public int readBytes(byte[] arg0, int arg1) {
-        // TODO Auto-generated method stub
-        return 0;
+	@Override
+    public byte readByte() throws JMSException {
+    	ensureReadMode();
+    	try {
+			return in.readByte();
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public char readChar() {
-        // TODO Auto-generated method stub
-        return 0;
+	@Override
+    public int readBytes(byte[] arg0) throws JMSException {
+    	ensureReadMode();
+    	try {
+			return in.read(arg0);
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public double readDouble() {
-        // TODO Auto-generated method stub
-        return 0;
+	@Override
+    public int readBytes(byte[] arg0, int arg1) throws JMSException {
+    	ensureReadMode();
+    	try {
+			return in.read(arg0, 0, arg1);
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public float readFloat() {
-        // TODO Auto-generated method stub
-        return 0;
+	@Override
+    public char readChar() throws JMSException {
+    	ensureReadMode();
+    	try {
+			return in.readChar();
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public int readInt() {
-        // TODO Auto-generated method stub
-        return 0;
+	@Override
+    public double readDouble() throws JMSException {
+    	ensureReadMode();
+    	try {
+			return in.readDouble();
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public long readLong() {
-        // TODO Auto-generated method stub
-        return 0;
+	@Override
+    public float readFloat() throws JMSException {
+    	ensureReadMode();
+    	try {
+			return in.readFloat();
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public short readShort() {
-        // TODO Auto-generated method stub
-        return 0;
+	@Override
+    public int readInt() throws JMSException {
+    	ensureReadMode();
+    	try {
+			return in.readInt();
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public String readUTF() {
-        // TODO Auto-generated method stub
-        return null;
+	@Override
+    public long readLong() throws JMSException {
+    	ensureReadMode();
+    	try {
+			return in.readLong();
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public int readUnsignedByte() {
-        // TODO Auto-generated method stub
-        return 0;
+	@Override
+    public short readShort() throws JMSException {
+    	ensureReadMode();
+    	try {
+			return in.readShort();
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public int readUnsignedShort() {
-        // TODO Auto-generated method stub
-        return 0;
+	@Override
+    public String readUTF() throws JMSException {
+    	ensureReadMode();
+    	try {
+			return in.readUTF();
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public void reset() {
-        // TODO Auto-generated method stub
-
+	@Override
+    public int readUnsignedByte() throws JMSException {
+    	ensureReadMode();
+    	try {
+			return in.readUnsignedByte();
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public void writeBoolean(boolean arg0) {
-        // TODO Auto-generated method stub
-
+	@Override
+    public int readUnsignedShort() throws JMSException {
+    	ensureReadMode();
+    	try {
+			return in.readUnsignedShort();
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public void writeByte(byte arg0) {
-        // TODO Auto-generated method stub
-
+	@Override
+    public void writeBoolean(boolean arg0) throws JMSException {
+    	ensureWriteMode();
+    	try {
+			out.writeBoolean(arg0);
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public void writeBytes(byte[] arg0) {
-        // TODO Auto-generated method stub
-
+	@Override
+    public void writeByte(byte arg0) throws JMSException {
+    	ensureWriteMode();
+    	try {
+			out.writeByte(arg0);
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public void writeBytes(byte[] arg0, int arg1, int arg2) {
-        // TODO Auto-generated method stub
-
+	@Override
+    public void writeBytes(byte[] arg0) throws JMSException {
+    	ensureWriteMode();
+    	try {
+			out.write(arg0);
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public void writeChar(char arg0) {
-        // TODO Auto-generated method stub
-
+	@Override
+    public void writeBytes(byte[] arg0, int arg1, int arg2) throws JMSException {
+    	ensureWriteMode();
+    	try {
+			out.write(arg0, arg1, arg2);
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public void writeDouble(double arg0) {
-        // TODO Auto-generated method stub
-
+	@Override
+    public void writeChar(char arg0) throws JMSException {
+    	ensureWriteMode();
+    	try {
+			out.writeChar(arg0);
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public void writeFloat(float arg0) {
-        // TODO Auto-generated method stub
-
+	@Override
+    public void writeDouble(double arg0) throws JMSException {
+    	ensureWriteMode();
+    	try {
+			out.writeDouble(arg0);
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public void writeInt(int arg0) {
-        // TODO Auto-generated method stub
-
+	@Override
+    public void writeFloat(float arg0) throws JMSException {
+    	ensureWriteMode();
+    	try {
+			out.writeFloat(arg0);
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public void writeLong(long arg0) {
-        // TODO Auto-generated method stub
-
+	@Override
+    public void writeInt(int arg0) throws JMSException {
+    	ensureWriteMode();
+    	try {
+			out.writeInt(arg0);
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public void writeObject(Object arg0) {
-        // TODO Auto-generated method stub
-
+	@Override
+    public void writeLong(long arg0) throws JMSException {
+    	ensureWriteMode();
+    	try {
+			out.writeLong(arg0);
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
-    public void writeShort(short arg0) {
-        // TODO Auto-generated method stub
-
+	@Override
+    public void writeObject(Object arg0) throws JMSException {
+    	if (null == arg0) throw new JMSException("attempt to write null object to message");
+    	else if (arg0 instanceof Boolean) writeBoolean((Boolean)arg0);
+    	else if (arg0 instanceof Byte) writeByte((Byte)arg0);
+    	else if (arg0 instanceof byte[]) writeBytes((byte[])arg0);
+    	else if (arg0 instanceof Character) writeChar((Character)arg0);
+    	else if (arg0 instanceof Double) writeDouble((Double)arg0);
+    	else if (arg0 instanceof Float) writeFloat((Float)arg0);
+    	else if (arg0 instanceof Integer) writeInt((Integer)arg0);
+    	else if (arg0 instanceof Long) writeLong((Long)arg0);
+    	else if (arg0 instanceof Short) writeShort((Short)arg0);
+    	else if (arg0 instanceof String) writeUTF((String)arg0);
+    	else throw new JMSException("attempt to write unknown type " + arg0.getClass().getName() + " to message");
     }
 
-    public void writeUTF(String arg0) {
-        // TODO Auto-generated method stub
-
+	@Override
+    public void writeShort(short arg0) throws JMSException {
+    	ensureWriteMode();
+    	try {
+			out.writeShort(arg0);
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
     }
 
+	@Override
+    public void writeUTF(String arg0) throws JMSException {
+    	ensureWriteMode();
+    	try {
+			out.writeUTF(arg0);
+		} catch (IOException e) {
+			throw new JMSException(e.getMessage());
+		}
+    }
+
+	@Override
 	public long getBodyLength() throws JMSException {
-		// TODO Auto-generated method stub
-		return 0;
+		return data.size();
 	}
 
 }
